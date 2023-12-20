6b7069d5472bdb756da46ccd7e3ac0dd38b04b47
GDelCorso <97095755+GDelCorso@users.noreply.github.com>
2023-12-20 17:09:11 +0100

    Add files via upload

--- test_NAD_giulio.py
+++ test_NAD_giulio.py
@@ -15,9 +15,11 @@ import NA_DAtabase as NA_D
 
 #%% Initialize the class
 
-my_sampler = NA_D.random_sampler(dataset_name='prova')
-
-my_sampler.import_dataset_properties()    
-
-my_sampler.call_sampler()
+#my_sampler = NA_D.random_sampler(dataset_name='prova')
+#my_sampler.import_dataset_properties()    
+#my_sampler.call_sampler()
+#my_sampler.generate_images()
 ###############################################################################
+
+NA_D.random_sampler(dataset_name='prova').auto_process()
+
