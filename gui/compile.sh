734b2fea380d622973ad743d791aac5a368bab47
Federico Volpini <volpini.federico79@gmail.com>
2024-10-04 18:58:19 +0200

    Delete gui directory

--- gui/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -R build
-rm -R dist
-pyinstaller --add-data=/home/federico/.local/lib/python3.8/site-packages/CTkColorPicker:CTkColorPicker --hidden-import PIL._tkinter_finder -F ./gui.py
